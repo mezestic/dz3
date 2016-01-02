6b808b0d5a4c8c4cf07bd0ce5438b972a2614cfc
mezestic <melitavz@hotmail.com>
2016-01-02 21:04:44 +0100

    Basic skeleton.

--- T2_09_zadaca_3/src/hr/foi/uzdiz/t2_09/zadaca3/mvc/Controller.java
+++ T2_09_zadaca_3/src/hr/foi/uzdiz/t2_09/zadaca3/mvc/Controller.java
@@ -11,4 +11,34 @@ package hr.foi.uzdiz.t2_09.zadaca3.mvc;
  */
 public class Controller {
     
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+    
+    public void run() {
+        String choice;
+        while(true) {
+            this.view.cleanScreen();
+            this.view.printMenu();
+            choice = this.view.requestChoice();
+            
+            this.executeChoice(choice);
+        }
+    }
+
+    private void executeChoice(String choice) {
+        switch (choice) {
+            case "":
+                
+                break;
+            default:
+                
+        }
+    }
+    
+    
 }
